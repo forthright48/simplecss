b62bb61049ed83c7792466eb581a11bcd665b245
Mohammad Samiul Islam <forthright48@gmail.com>
2016-07-14 23:39:33 +0600

    Add homepage for simplecss

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const app = express();
 const server = require('http').createServer(app);
 
 app.set('port', 8001);
+app.set('view engine', 'pug');
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use('/public/css', express.static(path.join(__dirname, '/src')));
 
+app.get('/', getHome);
 
 server.listen(app.get('port'), function() {
   console.log(`SimpleCSS server running at port ${app.get('port')}`);
 });
+
+
+/*******************************************
+Implementation
+*******************************************/
+
+function getHome(req, res) {
+  return res.render('home');
+}
